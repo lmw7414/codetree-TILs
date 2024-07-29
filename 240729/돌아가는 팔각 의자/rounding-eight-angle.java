6e5e78dac9e9b16b317d9142f33ab37e4d9183a1
Minwoo Lee <lmw7414@naver.com>
2024-07-29 15:52:45 +0900

    Title: 돌아가는 팔각 의자, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240729/돌아가는 팔각 의자/rounding-eight-angle.java
+++ 240729/돌아가는 팔각 의자/rounding-eight-angle.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 public class Main {
     static int[][] arr = new int[4][8];
+    static boolean[] visited;
     static int K;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,6 +18,7 @@ public class Main {
             String[] input = br.readLine().split(" ");
             int chairIdx = (input[0].charAt(0)- '0') - 1;
             int dir = input[1].charAt(0)- '0';
+            visited = new boolean[4];
             calc(chairIdx, dir);
         }
         System.out.println(getAnswer());
@@ -24,10 +26,10 @@ public class Main {
 
 
     public static void calc(int num, int dir) {
-        if(num < 0 || num > 3) return;
-        boolean left = num - 1 >= 0 && arr[num][6] != arr[num - 1][2]; // 왼쪽 방향 회전되는지 체크
-        boolean right = num + 1 < 4 && arr[num][2] != arr[num + 1][6];  // 오른쪽 방향 회전되는지 체크
+        boolean left = num - 1 >= 0 && arr[num][6] != arr[num - 1][2] && !visited[num - 1]; // 왼쪽 방향 회전되는지 체크
+        boolean right = num + 1 < 4 && arr[num][2] != arr[num + 1][6] && !visited[num + 1];  // 오른쪽 방향 회전되는지 체크
         setOctagonalChair(num, dir);  // 회전
+        visited[num] = true;
         if(left) {
             calc(num - 1, -dir);
             return;
