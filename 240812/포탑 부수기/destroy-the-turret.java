f9607337ae8120c548fe8da17ecb5c4dae13b2c4
Minwoo Lee <lmw7414@naver.com>
2024-08-12 22:52:52 +0900

    Title: 포탑 부수기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240812/포탑 부수기/destroy-the-turret.java
+++ 240812/포탑 부수기/destroy-the-turret.java
@@ -3,15 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/**
- * [문제 해결 프로세스]
- * 1. 가장 약한 포탑 찾기 -> 공격자 선정
- * 2. 가장 강한 포탑 찾기 -> 공격
- * 3. 레이저 공격 -> 우하좌상, 가장 강한 포탑 외 해당 경로의 포탑 역시 공격 대상이 됨
- * 4. 포탄 공격 -> 레이저 공격 불가 시.
- * -> 가장 강한 포탑의 공격력을 출력
- */
-
 public class Main {
     static int N, M, K;
     static int[][] arr, cntArr;
@@ -35,59 +26,58 @@ public class Main {
 
         for (int k = 0; k < K; k++) {
             relative = new boolean[N][M]; // 관련 없는 포탑 체크 배열
-            // 공격자 선정
-            Point attacker = findAttacker();
-            relative[attacker.x][attacker.y] = true;
-            // 공격대상 선정
-            Point strongest = strongestTower();
-            if (strongest.x == -1) break;
-            relative[strongest.x][strongest.y] = true;
+            // 공격자 & 공격 대상 선정
+            Point weaker = new Point(-1, -1);
+            Point stronger = new Point(-1, -1);
+            findWeakestAndStrongest(weaker, stronger);
+            relative[weaker.x][weaker.y] = true;
+            if (stronger.x == -1) break;
+            relative[stronger.x][stronger.y] = true;
+
             // 레이저 공격
-            arr[attacker.x][attacker.y] += N + M;
-            List<Point> result = laiserAttack(attacker, strongest);
-            if (result != null) {
-//                System.out.print("laiser path : ");
-//                for (Point p : result) {
-//                    System.out.print(p.x + " " + p.y + " ->");
-//                }
-//                System.out.println();
-                laserBomb(result, attacker, strongest);
-            } else { // 레이저 불가시 포탄 공격
-                bomb(attacker, strongest);
-            }
+            arr[weaker.x][weaker.y] += N + M;
+            List<Point> result = laserAttack(weaker, stronger);
+            if (result != null) laserBomb(result, weaker, stronger);
+            else bomb(weaker, stronger);
+
             repair(); // 공격과 무관한 포탄 정비 +1
-            addAttackTurn(attacker); // 턴 증가
-//            printArr(arr);
+            addAttackTurn(weaker); // 턴 증가
         }
 
         getResult();
     }
 
-    public static Point findAttacker() {
-        int power = 10000;
-        int bx = -1, by = -1;
-        for (int y = M - 1; y >= 0; y--) {
-            for (int x = N - 1; x >= 0; x--) {
+    public static void findWeakestAndStrongest(Point weaker, Point stronger) {
+        int wPower = 10000, sPower = 0;
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < M; y++) {
                 if (arr[x][y] == 0) continue;
-                if (power < arr[x][y]) continue;
-                if (bx == -1 && by == -1) {
-                    power = arr[x][y];
-                    bx = x;
-                    by = y;
-                    continue;
+                if (wPower >= arr[x][y]) {
+                    if (weaker.x == -1 && weaker.y == -1) {
+                        wPower = arr[x][y];
+                        weaker.x = x;
+                        weaker.y = y;
+                    } else if (isAttackBest(weaker, new Point(x, y))) {
+                        wPower = arr[x][y];
+                        weaker.x = x;
+                        weaker.y = y;
+                    }
                 }
-                if (isAttackBest(new Point(bx, by), new Point(x, y))) {
-                    power = arr[x][y];
-                    bx = x;
-                    by = y;
+                if (sPower <= arr[x][y]) {
+                    if (stronger.x == -1 && stronger.y == -1) {
+                        sPower = arr[x][y];
+                        stronger.x = x;
+                        stronger.y = y;
+                    } else if (isStrongest(stronger, new Point(x, y))) {
+                        sPower = arr[x][y];
+                        stronger.x = x;
+                        stronger.y = y;
+                    }
                 }
-
             }
         }
-
-//        System.out.println("attacker : " + bx + " " + by);
-        return new Point(bx, by);
     }
+
 
     public static boolean isAttackBest(Point best, Point cur) {
         if (arr[best.x][best.y] > arr[cur.x][cur.y]) return true;
@@ -103,37 +93,12 @@ public class Main {
         return false;
     }
 
-
     /*
      * 1. 공격력 가장 높음
      * 2. 공격한지 오래된 포탑
      * 3. 행과 열이 가장 작음
      * 4. 열 값이 가장 작음
      */
-    public static Point strongestTower() {
-        int power = 0;
-        int bx = -1, by = -1;
-        for (int y = 0; y < M; y++) {
-            for (int x = 0; x < N; x++) {
-                if (arr[x][y] == 0 || relative[x][y]) continue;
-                if (power > arr[x][y]) continue;
-                if (bx == -1 && by == -1) {
-                    power = arr[x][y];
-                    bx = x;
-                    by = y;
-                    continue;
-                }
-                if (isStrongest(new Point(bx, by), new Point(x, y))) {
-                    power = arr[x][y];
-                    bx = x;
-                    by = y;
-                }
-            }
-        }
-//        System.out.println("victim : " + bx + " " + by);
-        return new Point(bx, by);
-    }
-
     public static boolean isStrongest(Point best, Point cur) {
         if (arr[best.x][best.y] < arr[cur.x][cur.y]) return true;
         else if (arr[best.x][best.y] == arr[cur.x][cur.y]) {
@@ -148,9 +113,9 @@ public class Main {
         return false;
     }
 
-    public static List<Point> laiserAttack(Point start, Point victim) {
+    public static List<Point> laserAttack(Point start, Point victim) {
         int[][] pathArr = new int[N][M];
-        Point[][] pArr = new Point[N][M]; // 이전 것을 기록
+        Point[][] pArr = new Point[N][M]; // 이전 경로를 기록
         for (int i = 0; i < N; i++) Arrays.fill(pathArr[i], 100000);
         pathArr[start.x][start.y] = 0;
         int[] dx = {0, 1, 0, -1}; // 우하좌상
@@ -166,38 +131,38 @@ public class Main {
                 int ny = (M + cur.y + dy[d]) % M;
                 if (arr[nx][ny] == 0) continue;
                 if (pathArr[nx][ny] < pathArr[cur.x][cur.y] + 1) continue;
-                if(pArr[nx][ny] == null) {
-                    pArr[nx][ny] = new Point(cur.x, cur. y);
+                if (pArr[nx][ny] == null) {
+                    pArr[nx][ny] = new Point(cur.x, cur.y);
                 }
                 pathArr[nx][ny] = pathArr[cur.x][cur.y] + 1;
                 queue.add(new Point(nx, ny));
             }
         }
-//        printArr(pathArr);
         return findPath(victim, start, pArr);
     }
 
+    // 레이저 경로 탐색(공격대상으로 부터 공격자까지 이동)
     public static List<Point> findPath(Point start, Point dest, Point[][] path) {
         List<Point> list = new ArrayList<>();
         Point cur = start;
-        while(true) {
-           list.add(cur);
-           cur = path[cur.x][cur.y];
-           if (cur == null) return null;
-           if (cur.x == dest.x && cur.y == dest.y) return  list;
+        while (true) {
+            list.add(cur);
+            cur = path[cur.x][cur.y];
+            if (cur == null) return null;
+            if (cur.x == dest.x && cur.y == dest.y) return list;
         }
     }
 
-
+    // 레이저 경로에 있는 포탑 공격
     public static void laserBomb(List<Point> path, Point attack, Point victim) {
         int power = arr[attack.x][attack.y];
-        arr[victim.x][victim.y] -= power;
-        if (arr[victim.x][victim.y] < 0) arr[victim.x][victim.y] = 0;
         for (Point target : path) {
             relative[target.x][target.y] = true;
-            if (target.x == victim.x && target.y == victim.y) continue;
             if (target.x == attack.x && target.y == attack.y) continue;
+
+            if (target.x == victim.x && target.y == victim.y) arr[victim.x][victim.y] -= power; // 공격 대상자
             else arr[target.x][target.y] -= power / 2;
+            // 포탑 파괴되는 경우
             if (arr[target.x][target.y] < 0) arr[target.x][target.y] = 0;
         }
     }
@@ -216,10 +181,9 @@ public class Main {
             if (arr[nx][ny] < 0) arr[nx][ny] = 0;
             relative[nx][ny] = true;
         }
-
     }
 
-
+    // 공격과 무관한 포탑 정비
     public static void repair() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
@@ -238,9 +202,6 @@ public class Main {
                 else cntArr[i][j]++;
             }
         }
-//        System.out.println("cntArr");
-//        printArr(cntArr);
-//        System.out.println();
     }
 
     public static void getResult() {
@@ -251,16 +212,6 @@ public class Main {
             }
         }
         System.out.println(max);
-    }
-
-    public static void printArr(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if(arr[i][j] == 100000) System.out.print("INF ");
-                else System.out.print(arr[i][j] + "\t");
-            }
-            System.out.println();
-        }
     }
 
     static class Point {
