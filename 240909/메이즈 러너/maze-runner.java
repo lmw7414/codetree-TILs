4ccd3481a77524fa1c5c65e2f3dd65ae435e382f
Minwoo Lee <lmw7414@naver.com>
2024-09-09 01:20:15 +0900

    Title: 메이즈 러너, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240909/메이즈 러너/maze-runner.java
+++ 240909/메이즈 러너/maze-runner.java
@@ -65,9 +65,9 @@ public class Main {
             if(square == null) break;
             // 미로 회전
 
-            System.out.println(square[0] + " " + square[1] + " " + square[2]);
+            // System.out.println(square[0] + " " + square[1] + " " + square[2]);
             rotate(square[0], square[1], square[2]);
-            printArr();
+            // printArr();
         }
         System.out.println(answer);
         System.out.println((exit.x + 1) + " " + (exit.y + 1));
